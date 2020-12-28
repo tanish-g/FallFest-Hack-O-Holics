47233705918385746861ca364ef20c1c4ee60aba
sonali522 <76245085+sonali522@users.noreply.github.com>
2020-12-28 13:57:51 +0530

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -37,13 +37,18 @@ $(document).ready(function () {
             type: 'POST',
             url: '/predict',
             data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
             success: function (data) {
                 // Get and display the result
                 $('.loader').hide();
                 $('#result').fadeIn(600);
-               // $('#result').text(' Result:  ' + data);
-                console.log(data+'\n');
+                $('#result').text(' Result:  ' + data);
+                console.log(data+"\n");
                 $('.report').show();
+                $('.heading').show();
                 console.log('Success!');
             },
         });
